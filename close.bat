9aab418e4f640fca7d475e7b47fb9ad25ba6ca3c
xAJx <42112934+xAJx@users.noreply.github.com>
2023-09-15 19:11:06 +0800

    up

---
+++ close.bat
@@ -0,0 +1,3 @@
+git clone https://github.com/xAJx/Bat.git          # git clone 要下載的網址 .git
+cd ./Bat                                           # 要進入的 資料夾名稱
+C:/Users/jac13/anaconda3/python.exe test.py          # 透過 Terminal 隨便跑一個指令知道 [自己本機的-黃色路徑] (空格) 要跑的py檔名
---
+++ test.py
@@ -0,0 +1,2 @@
+print("Hi, I'm AJ")
+print("Congratulations, you are about to shut down")
