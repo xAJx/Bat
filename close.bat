68ede92e5abfac4e33d1def10bfd86b718e3434b
xAJx <42112934+xAJx@users.noreply.github.com>
2023-09-15 20:17:51 +0800

    up

--- close.bat
+++ close.bat
@@ -1,4 +1,7 @@
 git clone https://github.com/xAJx/Bat.git          
 cd ./Bat 
-C:/Users/jac13/anaconda3/python.exe test.py                                     
+C:/Users/jac13/anaconda3/python.exe test.py    
+cd ..
+cd ..
+shutdown -r -t 10                                 
          
