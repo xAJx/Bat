9e4d332046c06ba530f73fbca94e3b0ff2891c05
xAJx <42112934+xAJx@users.noreply.github.com>
2023-09-20 12:19:19 +0800

    Update test7.py

--- test7.py
+++ test7.py
@@ -1,12 +1,12 @@
 print("Hi, I'm AJ")
 print("Congratulations, you are about to shut down")
-print("cd ..")
-print("cd ..")
-print("cd ..")
-print("cd ..")
-print("shutdown -r -t 10")
-#input("cd ..")
+input("cd ..")
+input("cd ..")
+input("cd ..")
+input("cd ..")
+input("shutdown -r -t 10")
 input("")
+
 
 
 
